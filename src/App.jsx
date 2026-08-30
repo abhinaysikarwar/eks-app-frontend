516d03497c6faedad5fb7b605abf48672cd8d4eb
abhinaysikarwar <sikarwarlakhan00@gmail.com>
2026-08-30 11:32:44 +0530

    .env file update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_API_BASE_URL;
 
 function App() {
   const [message, setMessage] = useState("");
